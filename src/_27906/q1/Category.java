ef2919618e62b33a38f073c76e9941d36134c74a
27906 <mushimbonyumugara@gmail.com>
2025-11-19 17:45:17 +0200

    OOP assignment

---
+++ src/_27906/q1/Category.java
@@ -0,0 +1,21 @@
+package _27906.q1;
+import _27906.q1.Warehouse;
+
+public class Category extends Warehouse {
+    private String categoryName;
+    private String categoryCode;
+
+    public Category(int id, java.util.Date createdDate, java.util.Date updatedDate,
+                    String warehouseName, String location, String contactNumber,
+                    String categoryName, String categoryCode) {
+        super(id, createdDate, updatedDate, warehouseName, location, contactNumber);
+
+        if (categoryCode.length() < 3) throw new IllegalArgumentException("Code must be >= 3 chars");
+
+        this.categoryName = categoryName;
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() { return categoryName; }
+    public String getCategoryCode() { return categoryCode; }
+}
